4dd919905df25acdaeb76319a9c7b3af79c80971
hung20187237 <tranduchung308@gmail.com>
2022-07-26 17:41:55 +0700

    spks

--- FE/src/page/CreatePost/CreatePost.jsx
+++ FE/src/page/CreatePost/CreatePost.jsx
@@ -12,7 +12,8 @@ import {
     TabPanels,
     Tabs,
     Text,
-    Select
+    Select,
+    Img
   } from "@chakra-ui/react";
 import React from 'react'
 import FbImageLibrary from 'react-fb-image-grid'
@@ -35,10 +36,13 @@ export default function CreatePost() {
     const username = user.username;
     const [accounts, setAccounts] = useState([]);
     const [files, setFiles] = useState();
+    const [mutiupload, setMutiupload] = useState(null);
     const { user: currentUser, notifyFlag, dispatch } = useContext(Context);
     const content = useRef()
     const status = useRef()
     const account = useRef()
+    const access_token = useRef()
+
 
     useEffect(() => {
       const fetchAccounts = async () => {
@@ -53,6 +57,57 @@ export default function CreatePost() {
       fetchAccounts();
     }, [username]);
 
+    const postRandomQuote = async (e) => {
+        e.preventDefault();
+        const newData = {
+            access_token: access_token,
+            message: content.current.value,
+        }
+        const newData1 = {
+            access_token: access_token,
+            message: content.current.value,
+            link: mutifile[0],
+        }
+        if(mutifile.length === 1){
+            newData.url= mutifile; 
+        }
+        if(mutifile.length > 1){
+            newData1.child_attachments = [
+                mutifile.map( i => URL.createObjectURL(img))
+            ]
+        }
+
+ 
+        
+        try {
+            await axios.post("https://graph.facebook.com/100547109409842/photos?", newData)
+            .then(
+            res => {
+                const result = res.data;
+                console.log(result);
+                alert("Success!");
+            },
+                error => {
+                console.log(error);
+            })
+        }catch (err) { }
+
+        try {
+            await axios.post("https://graph.facebook.com/100547109409842/feed?", newData1)
+            .then(
+            res => {
+                const result = res.data;
+                console.log(result);
+                alert("Success!");
+            },
+                error => {
+                console.log(error);
+            })
+        }catch (err) { }
+    };
+
+
+
     const handlePostSubmit = async (e) => {
         e.preventDefault();
         const newPost = { userId: currentUser._id,
@@ -60,14 +115,14 @@ export default function CreatePost() {
             desc: content.current.value, 
             status: status.current.value 
         }
-        if (mutifile) {
+        if (mutiupload) {
             const data = new FormData();
             let fileName = [];
-            [...mutifile].map(file =>
+            mutiupload.map(file =>
                 data.append('images', file)
             )
 
-            console.log(newPost);
+            console.log(data);
             try {
                 await axios.post("http://localhost:8800/api/mutiupload", data)
                     .then(res =>
@@ -82,27 +137,25 @@ export default function CreatePost() {
             }
             catch (err) { }
         }
-        // if (source) {
-        //     const data = new FormData();
-        //     let fileName = [];
-        //     [...source].map(file =>
-        //         data.append('videos', file)
-        //     )
-
-        //     console.log(newPost);
-        //     try {
-        //         await axios.post("http://localhost:8800/api/mutiupload", data)
-        //             .then(res =>
-        //                 res.data
-        //             ).then(data =>
-        //                 data.file.map(file =>
-        //                     fileName.push(file.filename)
-        //                 )
-        //             )
-        //         newPost.video = Object.values(fileName);
-        //     }
-        //     catch (err) { }
-        // }
+        if (source) {
+            const data = new FormData();
+            let fileName = [];
+            source.map(file =>
+                data.append('videos', file)
+            )
+            try {
+                await axios.post("http://localhost:8800/api/mutiupload", data)
+                    .then(res =>
+                        res.data
+                    ).then(data =>
+                        data.file.map(file =>
+                            fileName.push(file.filename)
+                        )
+                    )
+                newPost.video = Object.values(fileName);
+            }
+            catch (err) { }
+        }
         try {
           console.log(newPost)
           await axios.post("http://localhost:8800/api/post", newPost);
@@ -116,161 +169,164 @@ export default function CreatePost() {
         const listImg =Object.values(files)
         const listUrl = listImg.map( img => URL.createObjectURL(img));
         setFiles(files)
+        setMutiupload(listImg)
         setMutifile(listUrl);
     }
     const handleFileChange = (files) => {
-        
         const listvideo = Object.values(files);
         setSource(listvideo);
-      };
+    };
 
 
-  return (
-    <>
-        <Topbar/>
-        <div className='homecontainer'>
-            <Sidebar/>
-            <div className='bodycreatepost'>
-                <div className='createpostleft'>
-                    <form className="reviewBottom">
-                        <div className='createpostTop'>
+    
 
-                            <Select placeholder= 'UserAccount'
-                                size={'lg'} 
-                                variant='filled'
-                                marginTop={'4vh'}
-                                width='300px'
-                                height={'50px'}
-                                padding={'10px 28px'}
-                                borderRadius='10px'
-                                ref={account}
-                            >
-                                    
-                                {accounts.map((a) => {
+
+    return (
+        <>
+            <Topbar/>
+            <div className='homecontainer'>
+                <Sidebar/>
+                <div className='bodycreatepost'>
+                    <div className='createpostleft'>
+                        <form className="reviewBottom">
+                            <div className='createpostTop'>
+
+                                <Select placeholder= 'UserAccount'
+                                    size={'lg'} 
+                                    variant='filled'
+                                    marginTop={'4vh'}
+                                    width='300px'
+                                    height={'50px'}
+                                    padding={'10px 28px'}
+                                    borderRadius='10px'
+                                    ref={account}
+                                >
+                                        
+                                    {accounts.map((a) => {
+                                        return (
+                                            <option value={a.accountId} ref = {a.access_token}>{a.accountname}</option>
+                                        
+                                        )
+                                    })} 
+                                </Select>
                                 
-                                    return (
-                                        <option value={a.accountId} >{a.accountname}</option>
-                                    
-                                    )
-                                })} 
+                                <button className='buttonselect'>Select teamviewer</button>
+                            </div>
+                            <div>
+                                <textarea
+                                    className="createpostInput"
+                                    placeholder="Post content..."
+                                    onChange={(e) => setDesc(e.target.value)}
+                                    ref={content}
+                                ></textarea>
+                            </div>
+
+                            <label htmlFor="file" className="addimagepost">
+                                <span className="addimageText">Add image</span>
+                                <input
+                                    style={{ display: "none" }}
+                                    type="file"
+                                    id="file"
+                                    multiple
+                                    accept=".png,.jpeg,.jpg"
+                                    onChange={(e) => {           
+                                        MutipleFileChange(e.target.files)
+                                    }}
+                                
+                                />
+                            </label>
+                            
+                            <label htmlFor="video" className="addvideopost">
+                                <span className="addimageText">Add video</span>
+                                <input
+                                    style={{ display: "none" }}
+                                    type="file"
+                                    id="video"
+                                    multiple
+                                    accept=".mp4,.avi,.vmv"
+                                    onChange={(e) => {
+                                        handleFileChange(e.target.files)
+                                    }}
+                                />
+                            </label>
+
+                            <Button marginTop={50} 
+                                height='40px'
+                                width={'70%'}
+                                border='1px solid'
+                                borderRadius={20}
+                                background={'rgb(200, 230, 255)'}
+                                onClick={handlePostSubmit}
+                            >Share</Button>
+                        </form>
+                    </div>
+                    <div className='createpostright'>
+                        <div className='createpostrightTop'>
+
+                            <Select placeholder='Status' 
+                                size={'lg'} 
+                                variant='outline'
+                                float={'right'}
+                                marginTop='4vh'
+                                width={'200px'}
+                                height='40px'
+                                borderRadius='10px'
+                                ref={status}
+                            >
+                                <option value='Published1'>Published</option>
+                                <option value='Scheduled'>Scheduled</option>
+                                <option value='Approcal'>Approcal</option>
+                                <option value='Failed'>Failed</option>
+                                <option value='Draft'>Draft</option>
                             </Select>
                             
-                            <button className='buttonselect'>Select teamviewer</button>
                         </div>
-                        <div>
-                            <textarea
-                                className="createpostInput"
-                                placeholder="Post content..."
-                                onChange={(e) => setDesc(e.target.value)}
-                                ref={content}
-                            ></textarea>
-                        </div>
-
-                        <label htmlFor="file" className="addimagepost">
-                            <span className="addimageText">Add image</span>
-                            <input
-                                style={{ display: "none" }}
-                                type="file"
-                                id="file"
-                                multiple
-                                accept=".png,.jpeg,.jpg"
-                                onChange={(e) => {           
-                                    MutipleFileChange(e.target.files)
-                                }}
-                            
-                            />
-                        </label>
-                        
-                        <label htmlFor="video" className="addvideopost">
-                            <span className="addimageText">Add video</span>
-                            <input
-                                style={{ display: "none" }}
-                                type="file"
-                                id="video"
-                                multiple
-                                accept=".mp4,.avi,.vmv"
-                                onChange={(e) => {
-                                    handleFileChange(e.target.files)
-                                }}
-                            />
-                        </label>
-
-                        <Button marginTop={50} 
-                            height='40px'
-                            width={'70%'}
-                            border='1px solid'
-                            borderRadius={20}
-                            background={'rgb(200, 230, 255)'}
-                            onClick={handlePostSubmit}
-                        >Share</Button>
-                    </form>
-                </div>
-                <div className='createpostright'>
-                    <div className='createpostrightTop'>
-
-                        <Select placeholder='Status' 
-                            size={'lg'} 
-                            variant='outline'
-                            float={'right'}
-                            marginTop='4vh'
-                            width={'200px'}
-                            height='40px'
-                            borderRadius='10px'
-                            ref={status}
-                        >
-                            <option value='Published1'>Published</option>
-                            <option value='Scheduled'>Scheduled</option>
-                            <option value='Approcal'>Approcal</option>
-                            <option value='Failed'>Failed</option>
-                            <option value='Draft'>Draft</option>
-                        </Select>
-                        
-                    </div>
-                    <Box className='postrightBody'>
-                        <span>Preview</span>
-                        <Box>
-                            {desc && (
-                            <Text textAlign={'left'} padding={'4px'}>{desc}</Text>)}
-                            {mutifile && (
-                            <Flex flexWrap={'wrap'} justifyContent= 'space-between'>
-                                <FbImageLibrary images={mutifile} countFrom={3} />
-                                <CancelIcon className="reviewCancelImg" onClick={() => setMutifile(null)} />
-                            </Flex>
-                            
-                            )}
-                            {source && (
+                        <Box className='postrightBody'>
+                            <span>Preview</span>
+                            <Box>
+                                {desc && (
+                                <Text textAlign={'left'} padding={'4px'}>{desc}</Text>)}
+                                {mutifile && (
                                 <Flex flexWrap={'wrap'} justifyContent= 'space-between'>
-                                    {source.map((vid)=> {
-                                        if(source.length === 1){
-                                            return (
-                                                <video
-                                                    className="VideoInput_video"
-                                                    width="86%"
-                                                    controls
-                                                    src={URL.createObjectURL(vid)}
-                                                />
-                                            )
-                                        }
-                                        if(source.length > 1){
-                                            return (
-                                                <video
-                                                    className="VideoInput_video1"
-                                                    controls
-                                                    src={URL.createObjectURL(vid)}
-                                                />
-                                            )
-                                        }
-                                    
-                                    })}
-                                    <CancelIcon className="reviewCancelImg" onClick={() => setSource(null)} />
+                                    <FbImageLibrary images={mutifile} countFrom={5} />
+                                    <CancelIcon className="reviewCancelImg" onClick={() => setMutifile(null)} />
                                 </Flex>
-                            )}
+                                
+                                )}
+                                {source && (
+                                    <Flex flexWrap={'wrap'} justifyContent= 'space-between'>
+                                        {source.map((vid)=> {
+                                            if(source.length === 1){
+                                                return (
+                                                    <video
+                                                        className="VideoInput_video"
+                                                        width="86%"
+                                                        controls
+                                                        src={URL.createObjectURL(vid)}
+                                                    />
+                                                )
+                                            }
+                                            if(source.length > 1){
+                                                return (
+                                                    <video
+                                                        className="VideoInput_video1"
+                                                        controls
+                                                        src={URL.createObjectURL(vid)}
+                                                    />
+                                                )
+                                            }
+                                        
+                                        })}
+                                        <CancelIcon className="reviewCancelImg" onClick={() => setSource(null)} />
+                                    </Flex>
+                                )}
+                            </Box>
                         </Box>
-                    </Box>
+                    </div>
                 </div>
+                
             </div>
-            
-        </div>
-    </>
-  )
+        </>
+    )
 }
+
